9ce433411c5e0ce19bfa7c32592a7f8b3927f9ec
tonyavoropaeva <voropaevatonya@gmail.com>
2025-02-28 20:48:36 +0700

    выполнила задачу Classes

---
+++ Classes/OptionByUsingES6/Animals.js
@@ -0,0 +1,31 @@
+"use strict";
+
+(function () {
+    class Animal {
+        constructor(name) {
+            this.name = name;
+        }
+
+        speak() {
+            console.log(this.name + " издает звук");
+        }
+    }
+
+    new Animal("Бедирхан").speak();
+
+    class Cat extends Animal {
+        speak() {
+            console.log(this.name + " мяукает");
+        }
+    }
+
+    new Cat("Мусенька").speak();
+
+    class Dog extends Animal {
+        speak() {
+            console.log(this.name + " гавкает");
+        }
+    }
+
+    new Dog("Джесенька").speak();
+})();
---
+++ Classes/OptionPrototype/Animals.js
@@ -0,0 +1,37 @@
+"use strict";
+
+(function () {
+    function Animal(name) {
+        this.name = name;
+    }
+
+    Animal.prototype.speak = function () {
+        console.log(this.name + " издает звук");
+    }
+
+    new Animal("Бедирхан").speak();
+
+    function Cat(name) {
+        Animal.call(this, name);
+    }
+
+    Cat.prototype = Object.create(Animal.prototype);
+
+    Cat.prototype.speak = function () {
+        console.log(this.name + " мяукает");
+    }
+
+    new Cat("Мурочка").speak();
+
+    function Dog(name) {
+        Animal.call(this, name);
+    }
+
+    Dog.prototype = Object.create(Animal.prototype);
+
+    Dog.prototype.speak = function () {
+        console.log(this.name + " гавкает");
+    }
+
+    new Dog("Кузенька").speak();
+})();
