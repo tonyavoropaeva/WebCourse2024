ebe0517c9c2425f00c4bac626930a1453ba67930
tonyavoropaeva <voropaevatonya@gmail.com>
2025-03-03 18:25:35 +0700

    выполнила задачу TodoList(еще дорабатываю)

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,16 +1,19 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", function () {
     const addTodoForm = document.getElementById("add-todo-form");
     const todoList = document.getElementById("todo-list");
     const newTodoTextField = document.getElementById("new-todo-text-field");
+    const addButton = document.getElementById("add-button");
 
     addTodoForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
         let newTodoText = newTodoTextField.value.trim();
-        newTodoTextField.classList.remove("invalid");
+        addButton.classList.remove("invalid");
 
         if (newTodoText.length === 0) {
-            newTodoTextField.classList.add("invalid");
+            addButton.classList.add("invalid");
             return;
         }
 
@@ -18,9 +21,12 @@ document.addEventListener("DOMContentLoaded", function () {
         newTodo.classList.add("todo-item");
 
         function setViewMode() {
-            newTodo.innerHTML = '<output class="todo-item-text"></output>' +
-                '<div class="buttons-group"><button class="delete-button" type="button"></button>' +
-                '<button class="edit-button" type="button"></button></div>';
+            newTodo.innerHTML =
+                '<output class="todo-item-text"></output>' +
+                '<div class="buttons-group">' +
+                '<button title="Удалить" class="delete-button" type="button"></button>' +
+                '<button title="Изменить" class="edit-button" type="button"></button>' +
+                '</div>';
 
             newTodo.querySelector(".todo-item-text").textContent = newTodoText;
 
@@ -29,10 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
             newTodo.querySelector(".edit-button").addEventListener("click", function () {
-                newTodo.innerHTML = '<input type="text" class="edit-text-field">' +
-                    '<div>Необходимо указать текст</div>' +
-                    '<div class="buttons-group"><button class="cancel-button" type="button"></button>' +
-                    '<button class="save-button" type="button"></button></div>';
+                newTodo.innerHTML =
+                    '<input type="text" class="edit-text-field">' +
+                    '<div class="buttons-group">' +
+                    '<button title="Отменить" class="cancel-button" type="button"></button>' +
+                    '<button title="Сохранить изменения" class="save-button" type="button"></button>' +
+                    '</div>';
 
                 const editTextField = newTodo.querySelector(".edit-text-field");
 
@@ -42,9 +50,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     setViewMode();
                 });
 
-                newTodo.querySelector(".edit-text-field").addEventListener("keypress", function (f) {
-                    if (f.keyCode === 13) {
-                        f.preventDefault();
+                newTodo.querySelector(".edit-text-field").addEventListener("keypress", function (enter) {
+                    if (enter.keyCode === 13) {
+                        enter.preventDefault();
 
                         newTodo.querySelector(".save-button").dispatchEvent(new Event("click", {
                             cancelable: true
@@ -57,11 +65,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
                     if (changedTodoText.length === 0) {
                         const errorMessage = document.createElement("div");
-                     //   document.getElementById("edit-text-field").classList.add("invalid");
                         errorMessage.textContent = "Необходимо указать текст";
                         errorMessage.className = "error-message-for-todo-input";
 
-                        if (newTodo.childNodes.length === 3) {
+                        if (newTodo.childNodes.length === 2) {
                             newTodo.appendChild(errorMessage);
                         }
 
