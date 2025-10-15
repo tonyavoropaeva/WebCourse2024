bf0d239cac67732edee0511272a77f6646b2eda7
tonyavoropaeva <voropaevatonya@gmail.com>
2025-10-15 20:30:08 +0700

    исправила Lodash

--- Lodash/script.js
+++ Lodash/script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import _ from "lodash";
-
 (function () {
     const persons = [
         {
@@ -52,44 +50,45 @@ import _ from "lodash";
             name: "Flor",
             age: 35
         }
-    ]
+    ];
 
     function getAverageAge(persons) {
-        return _
-            .chain(persons)
+        return _.chain(persons)
             .map("age")
-            .reduce((agesSum, age) => agesSum + age, 0)
-            .value() / persons.length;
+            .mean()
+            .value();
     }
 
+    console.log("Средний возраст: ");
     console.log(getAverageAge(persons));
 
-    function getPersonsFromTwentyToThirtyAgeInAscending(persons) {
-        return _
-            .chain(persons)
+    function getPersonsFromTwentyToThirtyAgeSortedInAscending(persons) {
+        return _.chain(persons)
             .filter(person => person.age >= 20 && person.age <= 30)
             .sortBy("age")
             .value();
     }
 
-    console.log(getPersonsFromTwentyToThirtyAgeInAscending(persons));
+    console.log("Люди от 20 до 30 отсортированные по возрастанию: ");
+    console.log(getPersonsFromTwentyToThirtyAgeSortedInAscending(persons));
 
-    function getUniqueNamesFromTwentyToThirtyAgeInDescending(persons) {
-        return _
-            .chain(persons)
+    function getUniqueNamesFromTwentyToThirtyAgeSortedInDescending(persons) {
+        return _.chain(persons)
             .filter(person => person.age >= 20 && person.age <= 30)
-            .sortBy("name")
             .map("name")
             .uniq()
+            .sortBy("name")
             .reverse()
             .value();
     }
 
-    console.log(getUniqueNamesFromTwentyToThirtyAgeInDescending(persons));
+    console.log("Люди от 20 до 30 отсортированные по убыванию: ");
+    console.log(getUniqueNamesFromTwentyToThirtyAgeSortedInDescending(persons));
 
-    function getCountNames(persons) {
+    function getNamesCounts(persons) {
         return _.countBy(persons, "name");
     }
 
-    console.log(getCountNames(persons));
+    console.log("Имена и количество их вхождений в список: ");
+    console.log(getNamesCounts(persons));
 })();
